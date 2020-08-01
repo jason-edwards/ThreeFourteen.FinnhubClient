74eb412e93b8a90635f10f008ae46fb7d6b7c6d4
jason-edwards <jedwards@live.com.au>
2020-08-01 12:42:29 +1000

    Commit

---
+++ src/ThreeFourteen.Finnhub.Client/Migrations/20200412073121_001.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ThreeFourteen.Finnhub.Client.Migrations
+{
+    public partial class _001 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApiRequests",
+                columns: table => new
+                {
+                    RequestId = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(maxLength: 50, nullable: true),
+                    RequestTime = table.Column<DateTime>(nullable: false),
+                    Weight = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiRequests", x => x.RequestId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiRequests");
+        }
+    }
+}
